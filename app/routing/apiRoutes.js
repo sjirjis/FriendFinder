92f9f34c35ffb20cd99b3490dad6b3b86a13607e
SJIRJIS <sjirjis@gmail.com>
2017-04-17 09:14:33 -0700

    initial commit

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,12 @@
+var path = require('path'),
+	friends = require('../data/friends');
+
+module.exports = function(app){
+	app.get('/api/friends', function(req, res){
+		res.json(friends);
+	});
+
+	app.post('/api/friends', function(req, res){
+		
+	})
+};
---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express'),
+	bodyParser = require('body-parser'),
+	app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+require('./app/routing/apiRoutes.js')(app);
+require('./app/routing/htmlRoutes.js')(app);
+
+
+app.listen(3000, function(){
+	console.log('App listening on PORT 3000');
+});
