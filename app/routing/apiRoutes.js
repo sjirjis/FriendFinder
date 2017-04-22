abb10146ae84d3aa00cdc90ea73dea94a454a6cf
SJIRJIS <sjirjis@gmail.com>
2017-04-22 09:59:26 -0700

    hw push

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,54 +1,54 @@
 var path = require('path'),
-	friends = require('../data/friends');
+    friends = require('../data/friends');
 
-module.exports = function(app){
-	app.get('/api/friends', function(req, res){
-		res.json(friends);
-	});
+module.exports = function(app) {
+    app.get('/api/friends', function(req, res) {
+        res.json(friends);
+    });
 
-	app.post('/api/friends', function(req, res){
-		
-		var userScores = req.body.scores,
-		totalDifference = 0,
-		matchComparisonScore = [],
-		bestMatch;
+    app.post('/api/friends', function(req, res) {
 
-		//parent loop for friends array
-		for (var f = 0; f < friends.length; f++){
+        var userScores = req.body.scores,
+            totalDifference = 0,
+            matchComparisonScore = [],
+            bestMatch;
 
-			totalDifference = 0;			
+        //parent loop for friends array
+        for (var f = 0; f < friends.length; f++) {
 
-			//child loop to set totalDifference for each friend
-			for (var i = 0; i < userScores.length; i++){
+            totalDifference = 0;
 
-				totalDifference += (Math.abs(userScores[i] - friends[f].scores[i]));
-			}				
+            //child loop to set totalDifference for each friend
+            for (var i = 0; i < userScores.length; i++) {
 
-			//add all friends to array
-			matchComparisonScore.push(friends[f]);
+                totalDifference += (Math.abs(userScores[i] - friends[f].scores[i]));
+            }
 
-			//add totaldifference to each friends object
-			matchComparisonScore[f].differenceScore = totalDifference;
-		}
+            //add all friends to array
+            matchComparisonScore.push(friends[f]);
 
-		//evaluate the best match based on differenceScore
-		var bestMatch = matchComparisonScore[0];
-		for (var m = 0; m < matchComparisonScore.length; m++){
+            //add totaldifference to each friends object
+            matchComparisonScore[f].differenceScore = totalDifference;
+        }
 
-			//does [m] have a lower differenceScore than current bestMatch...
-			 if (bestMatch.differenceScore > matchComparisonScore[m].differenceScore
+        //evaluate the best match based on differenceScore
+        var bestMatch = matchComparisonScore[0];
+        for (var m = 0; m < matchComparisonScore.length; m++) {
 
-			 	//and need a friend more than current bestMatch?
-			 	&& bestMatch.friendNeedness < matchComparisonScore[m].friendNeedness)
+            //does [m] have a lower differenceScore than current bestMatch...
+            if (bestMatch.differenceScore > matchComparisonScore[m].differenceScore
 
-			 	//if so we have a new match
-			 	bestMatch = matchComparisonScore[m];
-		}
+                //and need a friend more than current bestMatch?
+                && bestMatch.friendNeedness < matchComparisonScore[m].friendNeedness)
 
-		//dump the array for fresh values on the next post
-		matchComparisonScore = [];
+            //if so we have a new match
+                bestMatch = matchComparisonScore[m];
+        }
 
-		//and away we go
-		res.send(bestMatch);	
-	});
+        //dump the array for fresh values on the next post
+        matchComparisonScore = [];
+
+        //and away we go
+        res.send(bestMatch);
+    });
 };
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express'),
 	bodyParser = require('body-parser'),
 	app = express();
 
+var PORT = process.env.PORT || 3000;
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
@@ -12,7 +14,7 @@ app.use(express.static(__dirname + '/app/public'));
 require('./app/routing/apiRoutes.js')(app);
 require('./app/routing/htmlRoutes.js')(app);
 
-app.listen(3000, function(){
+app.listen(PORT, function(){
 	console.log('App listening on PORT 3000');
 });
 
